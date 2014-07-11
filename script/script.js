6bec7e273dc209964af8deadbe8452623049810f
Niccolò Cantù <niccolocantu94@hotmail.it>
2014-07-11 16:07:23 +0200

    it's just the beginning
    -->next = scrape

--- script/script.js
+++ script/script.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 });
 
 function sendRequest(){
+    url='http://www.italianvenue.com/expo-2015/news-expo/1/10';
     request=new XMLHttpRequest({mozSystem:true});
     request.open('GET',url,true);  
     request.send();
     request.onreadystatechange=function(){
         if(request.status == 200 && request.readyState == 4){
             response=request.responseText;
+            console.log(response);
             responseDoc=$.parseXML(response);
             $response=$(responseDoc);
         }  
